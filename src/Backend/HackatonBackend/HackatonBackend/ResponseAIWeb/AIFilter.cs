65b47a60d881e1c8b0efff3c5fc694c1bc8e78d1
Lena-hal <78831891+Olda-Hal@users.noreply.github.com>
2024-04-13 12:58:32 +0200

    AI filter for responses

---
+++ src/Backend/HackatonBackend/HackatonBackend/ResponseAIWeb/AIFilter.cs
@@ -0,0 +1,29 @@
+﻿namespace HackatonBackend.ResponseAIWeb
+{
+    public class AIFilter
+    {
+
+        private OpenAI_API.OpenAIAPI api { get; set; }
+        private String model { get; set; }
+        public AIFilter(string model, OpenAI_API.OpenAIAPI API)
+        {
+            api = API;
+            this.model = model;
+        }
+        public string Filtrate(List<string> responses, string question)
+        {
+            var chat = api.Chat.CreateConversation();
+            chat.Model = model;
+            chat.AppendSystemMessage("Jsi Filtr odpovědí na stránce BrnoID. dostaneš otázku a několik odpovědí a ty vrátíš pouze tu nejlepší odpověd s žadnym extra textem. Pokud by odpověd zněla nepravdivě nebo by nešlo určit jeji pravdivost rovnou ji zahoď. pokud odpověd neodpovida korektně nebo o brnu tak ji taky zahoď. pokud nezbyde žadná správná odpověd, vrať zprávu že v tuto chvíli nemůžeš odpovědět na tuto otazku");
+            string prompt = $"Otázka je: {question}. Možné odpovědi jsou:";
+            for (int i = 0; i < responses.Count; i++)
+            {
+                prompt += $"Odpověď {i + 1}: {responses[i]}.";
+            }
+            chat.AppendUserInput(prompt);
+            string res = chat.GetResponseFromChatbotAsync().Result;
+            return res;
+
+        }
+    }
+}
--- src/Backend/HackatonBackend/HackatonBackend/ResponseAIWeb/WebManager.cs
+++ src/Backend/HackatonBackend/HackatonBackend/ResponseAIWeb/WebManager.cs
@@ -1,4 +1,5 @@
 ﻿using HackatonBackend.DataSets;
+using OpenAI_API.Models;
 using TH = System.Threading.Tasks;
 
 namespace HackatonBackend.ResponseAIWeb
@@ -7,11 +8,24 @@ namespace HackatonBackend.ResponseAIWeb
     {
         public int density { get; set; }
         public List<string> plausibleModels { get; set; }
-        OpenAI_API.OpenAIAPI API = new OpenAI_API.OpenAIAPI("API_KEY");
+        public AIFilter filter { get; set; }
+        public string APIKey
+        {
+            get
+            {
+                string? Key = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("OpenAPIKey")["Key"];
+                char[] temp = Key!.ToCharArray();
+                Array.Reverse(temp);
+                string reversedKey = new string(temp);
+                return reversedKey;
+            }
+        }
+        public OpenAI_API.OpenAIAPI API { get; set; }
         public List<AIClient> clients { get; set; }
 
         public WebManager(int density=3, List<string> plausibleModels = null)
         {
+            API = new OpenAI_API.OpenAIAPI(APIKey);
             this.density = density;
             if (plausibleModels is null){
                 this.plausibleModels = new List<string>() { "gpt3-turbo" };
@@ -20,6 +34,7 @@ namespace HackatonBackend.ResponseAIWeb
             {
                 this.plausibleModels = plausibleModels;
             }
+            inicializeWeb();
             
 
         }
@@ -30,10 +45,10 @@ namespace HackatonBackend.ResponseAIWeb
                 // picks random model from plausiblemodels and creates a client
                 string model = plausibleModels[new Random().Next(plausibleModels.Count)];
                 clients.Add(new AIClient(model, API));
-                
             }
+            filter = new AIFilter("gpt-4-turbo", API);
         }
-        public List<string> GenerateResponse(string prompt)
+        public string GenerateResponse(string prompt, string question)
         {
             List<string> response = new List<string>();
             for (int i = 0; i < density; i++)
@@ -41,7 +56,7 @@ namespace HackatonBackend.ResponseAIWeb
                 response.Add(TH.Task.Run(() => clients[i].GenerateResponse(prompt)).Result);
                 
             }
-            return response;
+            return filter.Filtrate(response, question);
         }
         
     }
