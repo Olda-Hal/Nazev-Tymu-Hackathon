d22ab0c0580af4ff4ffcba44e7a654e9b391cf51
Lena-hal <78831891+Olda-Hal@users.noreply.github.com>
2024-04-13 12:34:05 +0200

    AI response web

---
+++ src/Backend/HackatonBackend/HackatonBackend/ResponseAIWeb/AIClient.cs
@@ -0,0 +1,25 @@
+﻿using OpenAI_API.Chat;
+using System.Globalization;
+namespace HackatonBackend.ResponseAIWeb
+{
+    public class AIClient
+    {
+        private OpenAI_API.OpenAIAPI api { get; set; }
+        private String model { get; set; }
+        public AIClient(string model, OpenAI_API.OpenAIAPI API)
+        {
+           api = API;
+           this.model = model;
+        }
+        public string GenerateResponse(string prompt)
+        {
+            var chat = api.Chat.CreateConversation();
+            chat.Model = model;
+            chat.AppendSystemMessage("Jsi assistent na strance BrnoID, mužeš odpovidat pouze na otazky tykajici se brna, pokud nemáš přesná data a nevíš přesně tak řekni nevím, nesmíš podávat chybné informace. Spolu s otázkou ti bude poskytnuto několik datasetů které byli vybrané tak aby ti pomohli odpovědět, použivej je na upřesnění otázky.");
+            chat.AppendUserInput(prompt);
+            string res = chat.GetResponseFromChatbotAsync().Result;
+            return res;
+        }
+
+    }
+}
---
+++ src/Backend/HackatonBackend/HackatonBackend/ResponseAIWeb/WebManager.cs
@@ -0,0 +1,48 @@
+﻿using HackatonBackend.DataSets;
+using TH = System.Threading.Tasks;
+
+namespace HackatonBackend.ResponseAIWeb
+{
+    public class WebManager
+    {
+        public int density { get; set; }
+        public List<string> plausibleModels { get; set; }
+        OpenAI_API.OpenAIAPI API = new OpenAI_API.OpenAIAPI("API_KEY");
+        public List<AIClient> clients { get; set; }
+
+        public WebManager(int density=3, List<string> plausibleModels = null)
+        {
+            this.density = density;
+            if (plausibleModels is null){
+                this.plausibleModels = new List<string>() { "gpt3-turbo" };
+            }
+            else
+            {
+                this.plausibleModels = plausibleModels;
+            }
+            
+
+        }
+        private void inicializeWeb()
+        {
+            for (int i = 0; i < density; i++)
+            {
+                // picks random model from plausiblemodels and creates a client
+                string model = plausibleModels[new Random().Next(plausibleModels.Count)];
+                clients.Add(new AIClient(model, API));
+                
+            }
+        }
+        public List<string> GenerateResponse(string prompt)
+        {
+            List<string> response = new List<string>();
+            for (int i = 0; i < density; i++)
+            {
+                response.Add(TH.Task.Run(() => clients[i].GenerateResponse(prompt)).Result);
+                
+            }
+            return response;
+        }
+        
+    }
+}
