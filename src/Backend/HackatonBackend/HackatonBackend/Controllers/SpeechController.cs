da53db6c2cf6765a9c3fcdb0aba42b97cfaf97d9
StanislavKrc <xkrcst00@stud.fit.vutbr.cz>
2024-04-13 22:39:11 +0200

    ADD - speech controler (romik cigik romik cigik romik cigik)

---
+++ src/Backend/HackatonBackend/HackatonBackend/Controllers/SpeechController.cs
@@ -0,0 +1,52 @@
+﻿using HackatonBackend.DataSets;
+using HackatonBackend.Models;
+using HackatonBackend.ResponseAIWeb;
+using Microsoft.AspNetCore.Mvc;
+namespace HackatonBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpeechController:ControllerBase
+    {
+        private readonly string _uploadFolderPath;
+
+        public SpeechController()
+        {
+            _uploadFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+            if (!Directory.Exists(_uploadFolderPath))
+            {
+                Directory.CreateDirectory(_uploadFolderPath);
+            }
+        }
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile([FromForm] FileUploadModel model)
+        {
+            if (model == null || model.File == null || model.File.Length == 0)
+            {
+                return BadRequest("Invalid file");
+            }
+            string uniqueFileName = Path.GetFileNameWithoutExtension(model.File.FileName) + "_" +
+                                    Guid.NewGuid().ToString() +
+                                    Path.GetExtension(model.File.FileName);
+            DataSetGetter dataSetGetter = new DataSetGetter();
+            List<DataSet> dataSets = dataSetGetter.GetMockSets();
+            string? input = await SpeechHandler.SpeechHandler.Transcript(model.File.FileName, true);
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return null;
+            }
+            DataSet bestDataSet = dataSetGetter.GetBestDataset(input, dataSets);
+            WebManager webManager = new WebManager();
+            // TODO fix this
+            string bestDatasetContent = System.IO.File.ReadAllText("C:\\Users\\petra\\Desktop\\hackathon\\Nazev-Tymu-Hackathon\\src\\Backend\\HackatonBackend\\HackatonBackend\\Data\\" + bestDataSet.Name);
+
+
+            string res = webManager.GenerateResponse(bestDatasetContent + input, input);
+            string resMP3Path = await SpeechHandler.SpeechHandler.TextToSpeech(res, uniqueFileName);
+
+            Console.WriteLine(res);
+            Console.WriteLine(resMP3Path);
+            return Ok(new { FilePath = resMP3Path });
+        }
+    }
+}
--- src/Backend/HackatonBackend/HackatonBackend/SpeechHandler/SpeechHandler.cs
+++ src/Backend/HackatonBackend/HackatonBackend/SpeechHandler/SpeechHandler.cs
@@ -13,11 +13,7 @@
         }
         public static async Task<string?> Transcript(string pathToMP3,bool deleteOld=false)
         {
-            var api=SpeechHandler.getAPI();
-            if(api==null)
-            {
-                throw new ArgumentNullException(nameof(api));
-            }
+            var api = getAPI();
             string result = await api.Transcriptions.GetTextAsync(pathToMP3);
             if (deleteOld)
             {
@@ -35,18 +31,33 @@
             }
             return result;
         }
-        public static async void TextToSpeech(string inputString,string outputPath,bool overwrite=true)
+        public static async Task<string> TextToSpeech(string inputString,string outputPath,bool overwrite=true)
         {
-            var api = SpeechHandler.getAPI();
-            if (api == null)
-            {
-                throw new ArgumentNullException(nameof(api));
-            }
-            if(File.Exists(outputPath)&&overwrite==false)
+            var api = getAPI();
+            if (File.Exists(outputPath)&&overwrite==false)
             {
                 throw new Exception("File already exist");
             }
             await api.TextToSpeech.SaveSpeechToFileAsync(inputString, outputPath);
+            if (!File.Exists(outputPath))
+            {
+                throw new Exception("File already exist");
+            }
+            return outputPath;
         }
+        public static async Task<string> GetSpeechResponse(string inputMP3Path,string outputMP3Path)
+        {
+            var api = getAPI();
+            string? str = await Transcript(api,inputMP3Path, true);
+            var chat = api.Chat.CreateConversation();
+            if (str==null)
+            {
+                throw new Exception("Failed to convert MP3 to string");
+            }
+            chat.Model = new OpenAI_API.Models.Model("gpt-4-turbo");
+
+            return await TextToSpeech(api, inputMP3Path, outputMP3Path);
+        }
+
     }
 }
