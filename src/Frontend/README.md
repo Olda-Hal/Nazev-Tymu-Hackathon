5fe3d2ef35755a276c902a3078386afee5129656
Lena-hal <78831891+Olda-Hal@users.noreply.github.com>
2024-04-14 08:23:42 +0200

    frontend

---
+++ src/Frontend/README.md
@@ -0,0 +1,17 @@
+# Hackathon Brno 2024
+
+## BrnoiD chatbot - Frontend
+
+### Getting started
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev 
+``` 
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/Frontend/components/navbar/RightSide.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../../styles/navbar/RightSide.module.scss';
+import FeatherIcon from 'feather-icons-react';
+
+interface RightSideProps {
+	strokeWidth?: number;
+}
+
+const RightSide: React.FC<RightSideProps> = ({ strokeWidth = 2 }) => {
+	return (
+		<ul className={styles['right-side']}>
+			<li className={styles['nav-item']}>
+				<Link href='#' className={styles['mobile']}>EN</Link>
+				<Link href='#' className={styles['desktop']}>English</Link>
+			</li>
+			<li className={styles['nav-item']}>
+				<Link href="http://localhost:3000/chat" passHref>
+					<FeatherIcon icon='help-circle' className={styles['icon']} size="25" strokeWidth={strokeWidth} />
+				</Link>
+				<Link href="http://localhost:3000/chat" passHref className={styles['desktop']}>Chatbot</Link>
+			</li>
+			<li className={styles['nav-item']}>
+				<Link href='#'>
+					<FeatherIcon icon='user' className={styles['icon']} size="25" strokeWidth={strokeWidth} />
+				</Link>
+				<Link href='#' className={styles['desktop']}>Přihlásit se</Link>
+			</li>
+			<li className={styles['nav-item']}>
+				<Link href='#'>
+					<FeatherIcon icon='shopping-cart' className={styles['icon']} size="25" strokeWidth={strokeWidth} />
+				</Link>
+			</li>
+			<li className={styles['nav-item']}>
+				<Link href='#'>
+					<FeatherIcon icon='align-left' className={`${styles['icon']} ${styles['icon-right']} ${styles['mobile']}`} size="19" strokeWidth={strokeWidth} />
+				</Link>
+			</li>
+		</ul >
+	);
+}
+
+export default RightSide;
